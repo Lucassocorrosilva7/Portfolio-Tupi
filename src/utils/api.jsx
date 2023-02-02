dd5967742615616fd7819f16f38893a7edff623d
lukecode <lucassocorrosilva@gmail.com>
2023-02-02 18:19:50 -0300

    Add novos conteúdos

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,19 +1,21 @@
-import React from "react";
 import { FiMapPin, FiBriefcase } from "react-icons/fi";
 
 export const aboutText = [
   {
-    a: `Desenvolvedor Front-end`,
+    id: 1,
+    text: `Desenvolvedor Front-end`,
     icon: <FiBriefcase />,
   },
   {
-    a: `São Paulo, Brasil`,
+    id: 2,
+    text: `São Paulo, Brasil`,
     icon: <FiMapPin />,
   },
 ];
 
 export const texts = [
   {
+    id: 1,
     text: `Olá, meu nome é Lucas Silva e sou desenvolvedor 
     front-end e UI designer. Gostaria de me apresentar e 
     compartilhar alguns fatos-chave sobre mim. Sou apaixonado 
@@ -28,4 +30,35 @@ export const skills = [
   { skill: `Javascript` },
   { skill: `Typescript` },
   { skill: `React` },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "BankSaintly",
+    description:
+      "Aplicação web desenvolvida para representar um banco digital moderno, feito em HTML, Css e JavaScript.",
+    url: "https://foodsaintly.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "LmCars",
+    description:
+      "Aplicação web desenvolvida para representar uma concessionária fictícia, feito React, Javascript, Sass, Axios, Vercel.",
+    url: `https://lmcars.vercel.app/`,
+  },
+  {
+    id: 3,
+    title: "FoodSaintly",
+    description:
+      "Aplicação web desenvolvida para representar um delivery moderno, feito em HTML, Sass, Gulp, JavaScript.",
+    url: "https://banksaintly.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "ImoInnovate",
+    description:
+      "Projeto ImoInnovate é uma plataforma imobiliária que foi desenvolvida com o objetivo de oferecer aos usuários uma experiência visual atraente e intuitiva.",
+    url: "https://imoinnovate.netlify.app/",
+  },
 ];
